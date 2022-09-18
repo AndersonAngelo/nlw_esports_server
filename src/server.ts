7fc85bf37754d964b671b211fbb92608012abfb2
AndersonAngelo <anderson_angelo1@hotmail.com>
2022-09-18 04:06:03 -0300

    Create database with SQlite

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import express from "express";
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+  return res.json([])
+})
+
+app.post('/ads', (req, res) => {
+  return res.json([])
+})
+
+app.get('/games/:id/ads', (req, res) => {
+  //const gamesId = req.params.id;
+
   return res.json([
     {id: 1, name: 'anúncio 1'},
     {id: 2, name: 'anúncio 2'},
@@ -10,5 +20,12 @@ app.get('/ads', (req, res) => {
     {id: 4, name: 'anúncio 4'}
   ])
 })
+
+app.get('/ads/:id/discord', (req, res) => {
+  //const adsId = req.params.id;
+
+  return res.json([])
+})
+
 
 app.listen(3333);
